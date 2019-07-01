1bad6eb8ecce08967d52f8f759ca491c32d5c232
Mitch <watchthepuppetshow@gmail.com>
2019-07-01 12:14:38 -0400

    Invoke ctypes for Windows scaling issue. Update README.MD

--- screen-shawty.py
+++ screen-shawty.py
@@ -1,18 +1,43 @@
 #!/usr/bin/env python3
 
+#################
+r'''
+S           .'|                     _     _   .-.          .-
+ C         <  |               /\    \\   //  .|\ \        / /
+  R         | |             __`\\  //\\ // .' |_\ \      / /
+   e     _  | | .'--.   .:--.'.\`//  \'/.'     |\ \    / /
+    e .' |  | |/.'-. \ / |   \ |\|   |/'--.  .-' \ \  / /
+    .N  | /|  /    | | `" __ | | '        |  |    \ `  /
+  .'.'| |//| |     | |  .'.''| |          |  |     \  /
+.'.'.-'  / | |     | | / /   | |_         |  '.'   / /
+.'   \_.'  | '.    | '.\ \._,\ '/         |   /|`-' /
+           '---'   '---'`--'  `"          `'-'  '..'
+'''
+#################
+
 import keyboard
 
-from PIL import ImageGrab
-
-from time import sleep
+from sys import platform
 from time import time
-
+from time import sleep
 from pathlib import Path
 
+# This is hacky, but has to be done to handle display scaling
+# This should be handled upstream by PIL
+# It also may need to be called BEFORE the import of PIL.ImageGrab
+if platform == 'win32':
+    from ctypes import windll
+    user32 = windll.user32
+    user32.SetProcessDPIAware()
+
+from PIL import ImageGrab
 import tkinter as tk
 
-VERSION = '0.01b'
-running = False # The application is stopped/not listening in background.
+#################
+
+VERSION = '0.02b' # TODO: __init__
+POLL_DELAY = 150 # ~6.66 polls for keyboard per second.
+running = False # Is the application polling keyboard input?
 
 #################
 
@@ -27,7 +52,10 @@ def start_stop():
         toggle_button.config(text="Start", bg='green')
 
 def take_screen_shot():
-    current_time = str(time()).replace('.','')
+    # time() returns a float, but we need to use it in a filename
+    current_time = str(time()).replace('.','_')
+
+    # Literally ~/Pictures/ (we'll see if it exists later...)
     picture_dir = Path.home().joinpath('Pictures')
 
     # State of the art error handling!
@@ -35,25 +63,28 @@ def take_screen_shot():
         try:
             picture_dir.mkdir(parents=True, exist_ok=True)
         except Exception as e:
-            print(e)
+            print(e) #TODO: implement logging module
 
     save_path = picture_dir.joinpath(f'Screen Shot {current_time}.png')
 
     screen_shot = ImageGrab.grab()
     screen_shot.save(save_path)
 
+    # We sleep here not to be lazy but to anticipate button_up
+    sleep(0.2)
+
 def shot_loop():
     if running:
         if keyboard.is_pressed('print screen'):
             take_screen_shot()
-    root.after(150, shot_loop)
+    root.after(POLL_DELAY, shot_loop)
 
 #################
 
 root = tk.Tk()
 
 root.title(f"screen-shawty {VERSION}")
-root.geometry("250x250")
+root.geometry("300x300")
 
 app = tk.Frame(root)
 app.pack(expand=True, fill='both')
@@ -62,5 +93,5 @@ toggle_button = tk.Button(app, text="Start", bg='green', font=(50), command=star
 
 toggle_button.pack(expand=True, fill='both')
 
-root.after(150, shot_loop)
+root.after(POLL_DELAY, shot_loop)
 root.mainloop()
